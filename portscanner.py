f33e25092f36933d58021ce1c0c78d10eb0b2ad0
NCPLoufer <85445165+NCPLoufer@users.noreply.github.com>
2021-07-16 16:36:52 +0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -6,10 +6,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(5)
 
 host = input('Enter the ip address you want to scan: ')
-#print('The ip address you want to scan is: ', host)
-
 port = int(input('Enter the port you want to scan: '))
-#print('The port you want to scan is: ', port)
 
 def portScanner(port):
     if s.connect_ex((host, port)):
